a298b900cb02f9c96859d0244026d0bd77cbb4af
Kaltenstein <dev@mercury-labs.de>
2024-06-16 11:34:35 +0200

    Setting up structure, added one-way sync for data/config

--- build.mjs
+++ build.mjs
@@ -1,12 +1,52 @@
 import { context } from 'esbuild';
 import { BitburnerPlugin } from 'esbuild-bitburner-plugin';
+import chokidar from chokidar;
+import fs from fs/promises;
+
+/** @type import('esbuild-bitburner-plugin').PluginExtension*/
+const oneWaySync = {
+  setup() {
+    console.log('Setting up Watcher');
+
+  }, //Run once on plugin startup
+
+  beforeConnect() { }, //Run once before the game connects
+  afterConnect(remoteAPI) {
+    let watcher = chokidar.watch(['./data', './config']);
+    watcher.on('add', path => pushFile(remoteAPI, path));
+    watcher.on('change', path => pushFile(remoteAPI, path)); //Run every time after the game (re)connects
+  },
+  beforeBuild() { }, //Run before every build process
+  afterBuild(remoteAPI) { }, //Run after build, before results are uploaded into the game
+};
+
+async function pushFile(remoteAPI, path) {
+  await fs.stat(path, (err, stat) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+  });
+  console.log(`Trying to push file ${path}`);
+  
+  let content = await fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+  }
+  );
+  return remoteAPI.pushFile({server: 'home', filename: path, content: content}).catch(err => { console.log(err); console.log(`${path} - ${content}`); });
+}
+
+
 
 const createContext = async () => await context({
   entryPoints: [
-    'servers/**/*.js',
-    'servers/**/*.jsx',
-    'servers/**/*.ts',
-    'servers/**/*.tsx',
+    'scripts/**/*.js',
+    'scripts/**/*.jsx',
+    'scripts/**/*.ts',
+    'scripts/**/*.tsx',
   ],
   outbase: "./servers",
   outdir: "./build",
@@ -14,6 +54,7 @@ const createContext = async () => await context({
     BitburnerPlugin({
       port: 12525,
       types: 'NetscriptDefinitions.d.ts',
+      plugins: [oneWaySync],
       mirror: {
       },
       distribute: {
