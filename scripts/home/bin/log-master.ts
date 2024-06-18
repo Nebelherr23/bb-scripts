2e62d87b218847055575c5cc7c9cf0a6f25f3d10
Nebelherr23 <138651224+Nebelherr23@users.noreply.github.com>
2024-06-18 09:32:42 +0200

    Logger now takes any form of LogLevel representation (#8)
    Log Levels can now be defined as string, number or LogLevel constant.
    (closes #4)

--- scripts/home/bin/log-master.ts
+++ scripts/home/bin/log-master.ts
@@ -4,6 +4,7 @@ export async function main(ns:NS) {
     ns.tail();
     ns.disableLog('ALL');
     const port = ns.getPortHandle(ns.pid);
+    ns.printf(`PID: ${ns.pid}`);
     ns.write('/tmp/log-master.txt',String(ns.pid),"w");
     port.clear();
     let running = true;
@@ -18,6 +19,7 @@ export async function main(ns:NS) {
     while (running) {
         if (port.empty()) await port.nextWrite();
         let logEvent:any = JSON.parse(port.read());
-        ns.tprintf(`[${logEvent.mod}:${coloredLogLevel(LogLevel[logEvent.level])}] ${logEvent.msg}`);
+        ns.printf(JSON.stringify(logEvent));
+        ns.tprintf(`[${logEvent.mod}:${coloredLogLevel(logEvent.level)}] ${logEvent.msg}`);
     }
 }
